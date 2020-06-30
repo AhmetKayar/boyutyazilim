c98c8c391d2903b236b0e9660a5ecdd53defd60e
Ahmet KAYAR <ahmet.kayra@gmail.com>
2020-06-30 21:54:23 +0300

    Project Initial commit

---
+++ src/test/java/com/boyutyazilim/boyutyazilim/BoyutyazilimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boyutyazilim.boyutyazilim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoyutyazilimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
