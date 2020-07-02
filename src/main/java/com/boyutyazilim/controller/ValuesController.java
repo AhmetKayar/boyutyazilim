a8351ffdab1933b82aca9e0ad5213ac73c3ff66d
Ahmet KAYAR <ahmet.kayra@gmail.com>
2020-07-02 20:00:55 +0300

    Docker Compose

--- src/main/java/com/boyutyazilim/controller/ValuesController.java
+++ src/main/java/com/boyutyazilim/controller/ValuesController.java
@@ -1,4 +1,27 @@
 package com.boyutyazilim.controller;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api", produces = {MediaType.APPLICATION_JSON_VALUE})
+
 public class ValuesController {
+
+    @CacheEvict(value = "values",allEntries = true)
+    @GetMapping("clean")
+    public ResponseEntity cleanCache(){
+
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("/values")
+    @Cacheable("values")
+    public ResponseEntity<String> values() {
+        return ResponseEntity.ok("Hello Word");
+    }
 }
